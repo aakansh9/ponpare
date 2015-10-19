ea3c3836c8312008bcff2ca412f015f2e217eace
Aakansh Gupta <aakansh-kddi@dhcp234.gat.cn.kddilabs.jp>
2015-10-19 19:30:38 +0900

    Updated for BPRMF model

--- RunMe.R
+++ RunMe.R
@@ -8,10 +8,15 @@ require(foreach)
 require(doMC)
 require(ROCR)
 
-setwd('[enter the path where you downloaded raw data]')
-source('code/clean.R') # 340 seconds
+# set path
+setwd('[enter the path where you downloaded repo]') # setwd('~/Projects/Recruit-Coupon-Purchase/github-code/')
+
+# run code
+source('code/clean.R') # ~ 340 seconds
 source('code/functions.R')
 source('code/featurize.R') # TAKES A LOT OF TIME AND CONTAINS PARALLEL CODE!
 source('code/model_xgboost.R')
 
 # for BPR Matrix Factorization using MyMediaLite library
+source('code/model_BPRMFknn.R')
+
